4e862989083f9d07e38902f4e10d9f8fd7c612e7
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-05-03 01:07:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('.body');
 const again = document.querySelector('.again');
 const guess = document.querySelector('.guess');
 const check = document.querySelector('.check');
@@ -5,27 +6,60 @@ const hint = document.querySelector('.hint');
 const score = document.querySelector('.score');
 const highscore = document.querySelector('.highscore');
 
-let randomNumber 
+let randomNumber;
+let scores = 19;
+let highscores = 18;
+
+window.addEventListener('DOMContentLoaded', function(){
+  randomNumber = Math.ceil(Math.random() * 20);
+  
+    score.innerHTML = 'Score:' + scores;
+    console.log(randomNumber);
+    guess.classList.remove('correct');
+})
+
 again.addEventListener('click', function(){
     randomNumber = Math.ceil(Math.random() * 20);
-    
-    guess.innerHTML = randomNumber;
+  
+    score.innerHTML = 'Score:' + 19;
+  
+    console.log(randomNumber);
 })
 
 
     check.addEventListener('click', function(){
         const val = document.getElementById('number').value; 
       if (randomNumber > +val) {
-         hint.innerHTML = "your guess is too low"
+         hint.innerHTML = "your guess is too low";
+         
+         scores -=1
+         score.innerHTML = 'score: ' + scores;
+         guess.classList.remove('correct')
       }
+
+
       else if (randomNumber < +val) {
-        hint.innerHTML = 'your guess is too high'
+        hint.innerHTML = 'your guess is too high';
+        scores -=1
+         score.innerHTML = 'score: ' + scores;
+         guess.classList.remove('correct')
+      }
+
+      else if (+val > 20) {
+        score.innerHTML = 'score: ' + scores;
+        hint.innerHTML = "Range exceeded!! kindly enter a number between 1 and 20";
+        
       }
 
       else if (randomNumber === +val) {
-        hint.innerHTML = "fantastic, you'll make a great gambler"
+        hint.innerHTML = "fantastic!!! You'll make a great gambler"; 
+         highscore.innerHTML = 'High' + score.innerHTML;
+
+         guess.innerHTML = randomNumber;
+         guess.classList.add('correct');
+         body.style.background = 'rgb(52, 186, 230)';
       }
-        
+       
     })
 
 
