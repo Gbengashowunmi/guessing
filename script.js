b4e0613950bbfe247c67a09540d0e7b07814bece
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-05-03 06:05:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const score = document.querySelector('.score');
 const highscore = document.querySelector('.highscore');
 
 let randomNumber;
-let scores = 19;
-let highscores = 18;
+let scores = 20;
+let highscores = 20;
 
 window.addEventListener('DOMContentLoaded', function(){
   randomNumber = Math.ceil(Math.random() * 20);
@@ -19,10 +19,10 @@ window.addEventListener('DOMContentLoaded', function(){
 })
 
 again.addEventListener('click', function(){
-    location.reload();
+    location.reload('default');
     randomNumber = Math.ceil(Math.random() * 20);
   
-    score.innerHTML = 'Score:' + scores;
+    score.innerHTML = 'Score:' + 20;
   
     console.log(randomNumber);
 })
@@ -30,26 +30,18 @@ again.addEventListener('click', function(){
 
     check.addEventListener('click', function(){
         const val = document.getElementById('number').value; 
-      if (randomNumber > +val) {
+
+       if (+val > 20) {
+          score.innerHTML = 'score: ' + scores;
+          hint.innerHTML = "Range exceeded!! kindly enter a number between 1 and 20"; 
+        }
+
+      else if (randomNumber > +val) {
          hint.innerHTML = "your guess is too low";
          
          scores -=1
          score.innerHTML = 'score: ' + scores;
          guess.classList.remove('correct')
-      }
-
-
-      else if (randomNumber < +val) {
-        hint.innerHTML = 'your guess is too high';
-        scores -=1
-         score.innerHTML = 'score: ' + scores;
-         guess.classList.remove('correct')
-      }
-
-      else if (+val > 20) {
-        score.innerHTML = 'score: ' + scores;
-        hint.innerHTML = "Range exceeded!! kindly enter a number between 1 and 20";
-        
       }
 
       else if (randomNumber === +val) {
@@ -60,6 +52,18 @@ again.addEventListener('click', function(){
          guess.classList.add('correct');
          body.style.background = 'rgb(52, 186, 230)';
       }
+
+      else if (randomNumber < +val) {
+        hint.innerHTML = 'your guess is too high';
+        scores -=1
+         score.innerHTML = 'score: ' + scores;
+         guess.classList.remove('correct')
+      }
+
+
+      
+
+     
        
     })
 
